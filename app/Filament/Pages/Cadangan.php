131de99702a8443dc3e3b5f16c72437bb15b1c87
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2026-07-26 15:03:16 +0800

    style: hide backup status table when no backups exist

--- app/Filament/Pages/Cadangan.php
+++ app/Filament/Pages/Cadangan.php
@@ -13,4 +13,15 @@ class Cadangan extends BaseBackups
     {
         return 'Cadangan';
     }
+
+    public function shouldDisplayStatusListRecords(): bool
+    {
+        $statusData = \ShuvroRoy\FilamentSpatieLaravelBackup\FilamentSpatieLaravelBackup::getBackupDestinationStatusData();
+        foreach ($statusData as $status) {
+            if ($status['amount'] > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
